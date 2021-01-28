c1f9a08ac84877fd7ec871681efe611328c30f8a
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-01-27 21:55:46 -0300

    Update

--- Threads - relógio/src/relogio/classe/Alarme.java
+++ Threads - relógio/src/relogio/classe/Alarme.java
@@ -1,5 +1,27 @@
 package relogio.classe;
 
-public class Alarme {
+public class Alarme implements Runnable {
 
+	Data data;
+	
+	public Alarme(Data data) {
+		this.data = data;
+	}
+
+	@Override
+	public void run() {
+		while(data.opcAlarme) {
+		
+			if(data.hora == data.aHora && data.minuto == data.aMinuto) {
+				System.out.println("ALARME!");
+				break;
+			}
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
