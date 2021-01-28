a7404d7e593f9ccb50de772fba251fc26593793c
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-01-27 21:52:39 -0300

    Update

--- Threads - relógio/src/relogio/classe/Cronometro.java
+++ Threads - relógio/src/relogio/classe/Cronometro.java
@@ -1,5 +1,36 @@
 package relogio.classe;
 
-public class Cronometro {
-
+public class Cronometro implements Runnable {
+	
+	Data data;
+	
+	public Cronometro(Data data) {
+		this.data = data;
+	}
+		
+	@Override
+	public void run() {
+		
+		while(data.opcCronometro) {
+		
+			data.cSegundo++;
+			
+			if(data.cSegundo==60) {
+				data.cSegundo=0;
+				data.cMinuto++;
+			}
+			if(data.cMinuto==60) {
+				data.cMinuto=0;
+				data.cHora++;
+			}
+			if(data.cHora==24) 
+				data.cHora=0;
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
